ff27330b0e0086eae56ad2e16f2d98f335394684
Benyamin Fallah <beny_micro@yahoo.com>
2024-07-22 22:42:39 +0330

    edit-01

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Friends from "./components/Friends";
+import FriendList from "./components/FriendList";
+import SplitBill from "./components/SplitBill";
+
+function App() {
+  const initialFriends = [
+    {
+      id: 118836,
+      name: "Clark",
+      image: "https://i.pravatar.cc/48?u=118836",
+      balance: -7,
+    },
+    {
+      id: 933372,
+      name: "Sarah",
+      image: "https://i.pravatar.cc/48?u=933372",
+      balance: 20,
+    },
+    {
+      id: 499476,
+      name: "Anthony",
+      image: "https://i.pravatar.cc/48?u=499476",
+      balance: 0,
+    },
+  ];
+
+  const [selectedTab, setSelectedTab] = useState();
+
+  return (
+    <React.Fragment>
+      <main>
+        <div className="app">
+          <div className="sidebar">
+            <ul>
+            <li>
+              <Friends {...initialFriends[0]} />
+              <button className="button">Select</button>
+              </li>
+              <li>
+              <Friends {...initialFriends[1]} />
+              <button className="button">Select</button>
+              </li>
+              <li>
+              <Friends {...initialFriends[2]} />
+              <button className="button">Select</button>
+              </li>
+            </ul>
+            {selectedTab ? <FriendList /> : null}
+            <button className="button" onClick={() => setSelectedTab("List")}>
+              {selectedTab ? "Close" : "Add Friend"}
+            </button>
+          </div>
+          <form className="form-split-bill">
+            <SplitBill />
+          </form>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+}
+export default App;
---
+++ src/components/Friends.jsx
@@ -0,0 +1,10 @@
+function Friends({ image, name, balance }) {
+    return (
+      <>
+                <img src={image} alt={name} />
+                <h3>{name}</h3>
+                <p>{balance}</p>
+              </>
+    );
+  }
+  export default Friends;
